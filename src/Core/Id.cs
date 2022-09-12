36ab3e0311c5b109b9e6025656d698a77e169524
ProphetLamb <prophet.lamb@gmail.com>
2022-09-13 00:09:46 +0200

    Reorganize project  according to Microsoft Specification

---
+++ src/Core/Id.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Surreal.NET;
+
+public static class Id
+{
+    public static string GetRandom(in int length)
+    {
+        Span<byte> buf = stackalloc byte[length];
+        Random.Shared.NextBytes(buf);
+        return Convert.ToHexString(buf);
+    }
+}
