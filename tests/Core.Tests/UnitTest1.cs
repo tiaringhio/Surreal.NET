318763d76ac4b9fc79b3b4e12f170559351fce03
ProphetLamb <prophet.lamb@gmail.com>
2022-09-13 00:59:38 +0200

    Define SURREAL_NET_INTERNAL for debug builds

--- tests/Core.Tests/UnitTest1.cs
+++ tests/Core.Tests/UnitTest1.cs
@@ -3,8 +3,8 @@ namespace Surreal.Net.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestRpcClient()
     {
-        
+        RpcClient client = new();
     }
 }
