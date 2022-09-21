2db7edd3245896b1308d802f0238203cb289f904
ProphetLamb <prophet.lamb@gmail.com>
2022-09-21 21:30:25 +0200

    Add docs to the example

--- examples/ConsoleRpc/Program.cs
+++ examples/ConsoleRpc/Program.cs
@@ -3,21 +3,26 @@ using SurrealDB.Driver.Rpc;
 using SurrealDB.Models;
 
 // start server: surreal start -b 0.0.0.0:8082 -u root -p root --log debug
+// Create a configuration for the sever specified above.
 Config cfg = Config.Create()
     .WithEndpoint("127.0.0.1:8082")
     .WithDatabase("test")
     .WithNamespace("test")
     .WithBasicAuth("root", "root")
+    // Tell the configuration to connect to the server using RPC.
     .WithRpc(insecure: true).Build();
 
+// Create a RPC database connection with the configuration.
 DatabaseRpc db = new(cfg);
+// Connect using the defined connection.
 await db.Open();
-
+// Crete a struct with the fields we want to insert, nesting is supported.
 Person you = new("Max Mustermann", 39, new("Musterstraße 1", 12345, "Musterstadt"), "0123456789", "max@mustermann.de");
-
-RpcResponse create = await db.Create("test:maxmustermann", you);
-
-RpcResponse select = await db.Select("test:maxmustermann");
+// Insert the struct into the database, table = person, id = maxmustermann.
+// If not id is specified a random one will be generated, the id can be read from the response.
+RpcResponse create = await db.Create("person:maxmustermann", you);
+// Read the struct from the database to verify it was inserted correctly.
+RpcResponse select = await db.Select("person:maxmustermann");
 if (select.TryGetResult(out Result result)) {
     // Prints: {"address":{"city":"Musterstadt","street":"Musterstraße 1","zip":12345},"age":39,"email":"max@mustermann.de","id":"test:maxmustermann","name":"Max Mustermann","phone":"0123456789"}
     Console.WriteLine(result.Inner);
@@ -27,5 +32,12 @@ if (select.TryGetResult(out Result result)) {
 }
 
 
+/// <summary>
+/// A Person.
+/// </summary>
 record struct Person(string name, int age, Address address, string phone, string email);
+
+/// <summary>
+/// The address of one or more people.
+/// </summary>
 record struct Address(string street, int zip, string city);
