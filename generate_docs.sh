848bbb77da61eb3f3b9220f8e9269531579b8625
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-09-21 21:18:22 +0200

    Setting up docs

---
+++ generate_docs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+declare -a StringArray=(
+  "Abstractions"
+  "Common"
+  "Configuration"
+  "Driver/Rest"
+  "Driver/Rpc"
+  "Extensions/Service"
+  "Json"
+  "Models"
+  "Ws"
+)
+
+
+for val in ${StringArray[@]}; do
+  stuff=($(echo $val | tr "/" "\n"))
+  last=${stuff[${#stuff[@]}-1]}
+  xmldocmd ./src/$val/bin/Release/net60/publish/$last.dll ./docs/src
+done
