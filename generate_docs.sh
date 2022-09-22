32cb8b433ea331671777093264a9bee8d8730878
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-09-22 11:07:41 +0200

    Added source

--- generate_docs.sh
+++ generate_docs.sh
@@ -16,5 +16,5 @@ declare -a StringArray=(
 for val in ${StringArray[@]}; do
   stuff=($(echo $val | tr "/" "\n"))
   last=${stuff[${#stuff[@]}-1]}
-  xmldocmd ./src/$val/bin/Release/net60/publish/$last.dll ./docs/src
+  xmldocmd ./src/$val/bin/Release/net60/publish/$last.dll ./docs/src --source https://github.com/ProphetLamb/Surreal.Net/tree/master/src/$val
 done
