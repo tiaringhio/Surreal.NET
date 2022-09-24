5a55baba90a800b9e601a8f8226e132ceea87e8c
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-09-24 16:40:09 +0200

    Debugging

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+set -e
+
 declare -a StringArray=(
   "Abstractions"
-  "Common"
-  "Configuration"
-  "Driver/Rest"
-  "Driver/Rpc"
-  "Extensions/Service"
-  "Json"
-  "Models"
-  "Ws"
+  # "Common"
+  # "Configuration"
+  # "Driver/Rest"
+  # "Driver/Rpc"
+  # "Extensions/Service"
+  # "Json"
+  # "Models"
+  # "Ws"
 )
 
 
@@ -18,7 +20,12 @@ for val in ${StringArray[@]}; do
   last=${stuff[${#stuff[@]}-1]}
   path=./src/$val/bin/Release/net60/publish
 
+  echo "Running: $path/$last.pdb $last-meta.json src/"
   extract $path/$last.pdb $last-meta.json src/
+
+  echo "Running: mapper $last-meta.json src/ $last-symbols.json"
   mapper $last-meta.json src/ $last-symbols.json
+
+  echo "Running: xmldocmd $path/$last.dll ./docs/src --source src/ --symbols $last-symbols.json"
   xmldocmd $path/$last.dll ./docs/src --source src/ --symbols $last-symbols.json
 done
