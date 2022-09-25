0f653dfc1a0d9f83ad57753891ccfe56be5f4e78
ProphetLamb <prophet.lamb@gmail.com>
2022-09-25 10:49:08 +0200

    Alias Random.Shared for >=NET60

--- src/Common/ThreadRng.cs
+++ src/Common/ThreadRng.cs
@@ -1,8 +1,12 @@
 namespace SurrealDB.Common;
 
 public class ThreadRng {
+#if NET6_0_OR_GREATER
+    public static Random Shared => Random.Shared;
+#else
     [ThreadStatic]
     private static Random? s_shared;
 
     public static Random Shared => s_shared ?? new();
+#endif
 }
