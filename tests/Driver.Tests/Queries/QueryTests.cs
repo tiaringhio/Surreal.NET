fdaa6e5ff74f5bb2860cb85e20a481dc0ad685d3
Du-z <16366766+Du-z@users.noreply.github.com>
2022-09-25 21:25:20 +1000

    Added a few more roundtrip data types

--- tests/Driver.Tests/Queries/QueryTests.cs
+++ tests/Driver.Tests/Queries/QueryTests.cs
@@ -78,14 +78,3 @@ public abstract class QueryTests<T, U, TKey, TValue>
         doc.Should().BeEquivalentTo(expectedObject);
     }
 }
-
-public class TestObject<TKey, TValue> {
-    [JsonConstructor]
-    public TestObject(TKey key, TValue value) {
-        Key = key;
-        Value = value;
-    }
-
-    public TKey Key { get; set; }
-    public TValue Value { get; set; }
-}
--- tests/Driver.Tests/Roundtrip/RoundTripTests.cs
+++ tests/Driver.Tests/Roundtrip/RoundTripTests.cs
@@ -93,12 +93,14 @@ public class RoundTripObject {
     public int ZeroInteger { get; set; } = 0;
     public int MaxInteger { get; set; } = int.MaxValue;
     public int MinInteger { get; set; } = int.MinValue;
+    public int? NullInteger { get; set; } = null;
 
     public long PositiveLong { get; set; } = long.MaxValue / 2;
     public long NegativeLong { get; set; } = long.MinValue / 2;
     public long ZeroLong { get; set; } = 0;
     public long MaxLong { get; set; } = long.MaxValue;
     public long MinLong { get; set; } = long.MinValue;
+    public long? NullLong { get; set; } = null;
 
     public float PositiveFloat { get; set; } = float.MaxValue / 7;
     public float NegativeFloat { get; set; } = float.MinValue / 7;
@@ -110,6 +112,7 @@ public class RoundTripObject {
     public float NegEpsilonFloat { get; set; } = -float.Epsilon;
     public float PositiveInfinityFloat { get; set; } = float.PositiveInfinity; // Not Supported by default by System.Text.Json
     public float NegativeInfinityFloat { get; set; } = float.NegativeInfinity; // Not Supported by default by System.Text.Json
+    public float? NullFloat { get; set; } = null;
 
     public double PositiveDouble { get; set; } = double.MaxValue / 7;
     public double NegativeDouble { get; set; } = double.MinValue / 7;
@@ -121,32 +124,63 @@ public class RoundTripObject {
     public double NegEpsilonDouble { get; set; } = -double.Epsilon;
     public double PositiveInfinityDouble { get; set; } = double.PositiveInfinity; // Not Supported by default by System.Text.Json
     public double NegativeInfinityDouble { get; set; } = double.NegativeInfinity; // Not Supported by default by System.Text.Json
+    public double? NullDouble { get; set; } = null;
 
     public decimal PositiveDecimal { get; set; } = decimal.MaxValue / 7m;
     public decimal NegativeDecimal { get; set; } = decimal.MinValue / 7m;
     public decimal ZeroDecimal { get; set; } = 0;
     public decimal MaxDecimal { get; set; } = decimal.MaxValue;
     public decimal MinDecimal { get; set; } = decimal.MinValue;
+    public decimal? NullDecimal { get; set; } = null;
 
     public DateTime MaxUtcDateTime { get; set; } = DateTime.MaxValue.AsUtc();
     public DateTime MinUtcDateTime { get; set; } = DateTime.MinValue.AsUtc();
-    public DateTime NowUtcDateTime { get; set; } = DateTime.Now.AsUtc();
+    public DateTime NowUtcDateTime { get; set; } = DateTime.UtcNow;
+    public DateTime? NullDateTime { get; set; } = null;
+
+    public DateTimeOffset MaxUtcDateTimeOffset { get; set; } = DateTimeOffset.MaxValue.ToUniversalTime();
+    public DateTimeOffset MinUtcDateTimeOffset { get; set; } = DateTimeOffset.MinValue.ToUniversalTime();
+    public DateTimeOffset NowUtcDateTimeOffset { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? NullDateTimeOffset { get; set; } = null;
+
+    public DateOnly MaxUtcDateOnly { get; set; } = DateOnly.MaxValue;
+    public DateOnly MinUtcDateOnly { get; set; } = DateOnly.MinValue;
+    public DateOnly NowUtcDateOnly { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+    public DateOnly? NullUtcDateOnly { get; set; } = null;
+
+    public TimeOnly MaxUtcTimeOnly { get; set; } = TimeOnly.MaxValue;
+    public TimeOnly MinUtcTimeOnly { get; set; } = TimeOnly.MinValue;
+    public TimeOnly NowUtcTimeOnly { get; set; } = TimeOnly.FromDateTime(DateTime.UtcNow);
+    public TimeOnly? NullUtcTimeOnly { get; set; } = null;
 
     public Guid Guid { get; set; } = Guid.NewGuid();
     public Guid EmptyGuid { get; set; } = Guid.Empty;
+    public Guid? NullGuid { get; set; } = null;
 
     public bool TrueBool { get; set; } = true;
     public bool FalseBool { get; set; } = false;
+    public bool? NullBool { get; set; } = null;
 
     public StandardEnum ZeroStandardEnum { get; set; } = StandardEnum.Zero;
     public StandardEnum OneStandardEnum { get; set; } = StandardEnum.One;
     public StandardEnum TwoHundredStandardEnum { get; set; } = StandardEnum.TwoHundred;
     public StandardEnum NegTwoHundredStandardEnum { get; set; } = StandardEnum.NegTwoHundred;
+    public StandardEnum? NullStandardEnum { get; set; } = null;
 
     public FlagsEnum NoneFlagsEnum { get; set; } = FlagsEnum.None;
     public FlagsEnum AllFlagsEnum { get; set; } = FlagsEnum.All;
     public FlagsEnum SecondFourthFlagsEnum { get; set; } = FlagsEnum.Second | FlagsEnum.Fourth;
     public FlagsEnum UndefinedFlagsEnum { get; set; } = (FlagsEnum)(1 << 8);
+    public FlagsEnum? NullFlagsEnum { get; set; } = null;
+
+     public TestObject<int, int>? TestObject { get; set; } = new TestObject<int, int>(-100, 1);
+     public TestObject<int, int>? NullTestObject { get; set; } = null;
+
+    public int[] IntArray { get; set; } = new [] {-100, 1, 0, -1, 100};
+    public int[]? NullIntArray { get; set; } = null;
+
+    public TestObject<int, int>?[] TestObjectArray { get; set; } = new [] { new TestObject<int, int>(-100, 1), new TestObject<int, int>(0, -1), null };
+    public TestObject<int, int>?[]? NullTestObjectArray { get; set; } = null;
 
     public static void AssertAreEqual(
         RoundTripObject? a,
