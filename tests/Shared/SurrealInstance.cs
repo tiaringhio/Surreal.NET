95be640b467e5a9bc5f1c7f614643d4208698770
Du-z <16366766+Du-z@users.noreply.github.com>
2022-09-26 21:30:40 +1000

    Fixed tests on windows not finding the TCP connection

--- tests/Shared/SurrealInstance.cs
+++ tests/Shared/SurrealInstance.cs
@@ -3,7 +3,7 @@ namespace SurrealDB.Shared.Tests;
 public static class SurrealInstance {
     public static Process Create() {
         // Wait until port is closed
-        TcpHelper.SpinUntilClosed($"0.0.0.0:{TestHelper.Port}");
+        TcpHelper.SpinUntilClosed($"{TestHelper.Loopback}:{TestHelper.Port}");
         // We have a race condition between when the SurrealDB socket opens (or closes), and when it accepts connections.
         // 50ms should be enough to run the tests on most devices.
         // Thread.Sleep(50);
@@ -14,7 +14,7 @@ public static class SurrealInstance {
         // Start new instances
         var p = Instantiate(path!);
         Debug.Assert(p is not null);
-        TcpHelper.SpinUntilListening($"0.0.0.0:{TestHelper.Port}");
+        TcpHelper.SpinUntilListening($"{TestHelper.Loopback}:{TestHelper.Port}");
         // Thread.Sleep(50);
         Debug.Assert(!p.HasExited);
         return p;
@@ -22,9 +22,10 @@ public static class SurrealInstance {
 
     private static Process? Instantiate(string path) {
         ProcessStartInfo pi = new() {
-            UseShellExecute = true,
+            CreateNoWindow = true,
+            UseShellExecute = false,
             FileName = path,
-            Arguments = $"start memory -b 0.0.0.0:{TestHelper.Port} -u {TestHelper.User} -p {TestHelper.Pass} --log debug",
+            Arguments = $"start memory -b {TestHelper.Loopback}:{TestHelper.Port} -u {TestHelper.User} -p {TestHelper.Pass} --log debug",
             WorkingDirectory = Environment.CurrentDirectory
         };
         return Process.Start(pi);
